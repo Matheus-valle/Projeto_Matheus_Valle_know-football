33cc575b69ea428a0d025a29dd6212615df4d06f
Matheus-valle <matheus.valle@sptech.school>
2025-11-19 21:50:20 -0300

    readequação site

---
+++ SITE/FRONT-END/src/models/usuarioModel.js
@@ -0,0 +1,43 @@
+var database = require("../database/config")
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT idUsuario, nome_usuario, email_usuario FROM usuario WHERE email_usuario = '${email}' AND senha_usuario = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nome, email, senha, fkEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO Usuario (nome_usuario, email_usuario, senha_usuario) VALUES ('${nome}', '${email}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+
+function continuacao_cadastro(região_usuario, altura_usuario, peso_usuario, frequencia_treino){
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
+    
+    var instrucaoSql = `
+       UPDATE Usuario SET região_usuario = '${região_usuario}', altura_usuario = ${altura_usuario}, peso_usuario = ${peso_usuario}, frequencia_treino = ${frequencia_treino}
+    WHERE email_usuario = '${email_usuario}' AND nome_usuario = '${nome_usuario}'; 
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    autenticar,
+    cadastrar,
+    continuacao_cadastro
+};
