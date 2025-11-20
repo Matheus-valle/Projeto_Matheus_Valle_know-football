4e61f13638c92e09694d8ebad04d3d4fd4fbafb9
Matheus-valle <matheus.valle@sptech.school>
2025-11-20 08:52:18 -0300

    mudanças modelagem

--- SITE/FRONT-END/src/models/usuarioModel.js
+++ SITE/FRONT-END/src/models/usuarioModel.js
@@ -24,20 +24,23 @@ function cadastrar(nome, email, senha, regiao, altura, peso, frequencia) {
 
 
 
-function continuacao_cadastro(região_usuario, altura_usuario, peso_usuario, frequencia_treino){
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
+function cadastrar_metricas(nome, email, senha, regiao, altura, peso, frequencia) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-       UPDATE Usuario SET região_usuario = '${região_usuario}', altura_usuario = ${altura_usuario}, peso_usuario = ${peso_usuario}, frequencia_treino = ${frequencia_treino}
-    WHERE email_usuario = '${email_usuario}' AND nome_usuario = '${nome_usuario}'; 
+        INSERT INTO Usuario (nome_usuario, email_usuario, senha_usuario, região_usuario, altura_usuario, peso_usuario, frequencia_treino) VALUES ('${nome}', '${email}', '${senha}', '${regiao}', ${altura}, ${peso}, ${frequencia});
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 
+
+
 module.exports = {
     autenticar,
     cadastrar,
-    continuacao_cadastro
+    cadastrar_metricas
 };
