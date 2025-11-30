d98767fa6b6acc8f73f48446ddd202b26d9823a0
Matheus-valle <matheus.valle@sptech.school>
2025-11-30 03:37:34 -0300

    finalização kpis

--- BANCO DE DADOS/script_knowFootball.sql
+++ BANCO DE DADOS/script_knowFootball.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE football_db;
-
 USE football_db;
 
 
@@ -30,11 +29,23 @@ formulario_time VARCHAR(200),
 categoria VARCHAR(45)
 ); 
 
+INSERT INTO TIMES (nome_time, estado_time, região_time, cidade_time, formulario_time, categoria) VALUES
+	('Time Manaus', 'Amazonas', 1, 'Manaus' ,'asdsalasdk', 'masculino'),
+	('Time Pará', 'Pará', 1, 'Belem' ,'asdsalasdk', 'feminino'),
+	('Time Recife', 'Pernambuco', 2, 'Recife', 'asdsalasdk', 'masculino'),
+	('Time Fortaleza', 'Ceara', 2,'Fortaleza' ,'asdsalasdk', 'feminino'),
+	('Time Cuiaba', 'Mato Grosso', 3,'Cuiaba' ,'asdsalasdk', 'masculino'),
+	('Time Campo Grande', 'Mato Grosso do SUl', 3, 'Campo grande', 'asdasda', 'feminino'),  
+	('Time São Paulo', 'São Paulo', 4, 'São Paulo', 'asdasda', 'masculino'),  
+	('Time Belo Horizonte', 'Minas Gerais', 4, 'Belo Horizonte', 'asdasda', 'feminino'),  
+	('Time Porto Alegre', 'Rio Grande do sul', 5, 'Porto alegre', 'asdasda', 'masculino'),  
+	('Time Curitiba', 'Parana', 5, 'Curitiba', 'asdasda', 'feminino');  
 
+SELECT * FROM TIMES WHERE região_time = 3;
 CREATE TABLE Usuario (
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nome_usuario VARCHAR(45) NOT NULL, 
-    email_usuario VARCHAR(45) NOT NULL,
+    email_usuario VARCHAR(45) NOT NULL UNIQUE,
     senha_usuario VARCHAR(45) NOT NULL,
     região_usuario INT,
     CONSTRAINT fk_região_usuario
@@ -54,7 +65,7 @@ CREATE TABLE Usuario (
 CREATE TABLE Metrica_atual (
 idMetrica INT AUTO_INCREMENT,
 fk_usuario INT,
-mes INT NOT NULL,  
+mes INT NOT NULL UNIQUE,  
 metrica_tiro40y DECIMAL(3,2),
 metrica_3cones DECIMAL(4,2),
 metrica_shuttle_20 DECIMAL(4,2),
@@ -64,11 +75,11 @@ CONSTRAINT pks_metrica_fkusuario
 	PRIMARY KEY(idMetrica, fk_usuario),
 CONSTRAINT fk_usuario_metrica_atual
 	FOREIGN KEY(fk_usuario)
-		REFERENCES Usuario (idUsuario)
+		REFERENCES Usuario (idUsuario),
+CONSTRAINT unico_usuario_e_mes
+		UNIQUE (fk_usuario, mes)
 );
-SELECT * FROM Usuario;
 
-DESCRIBE Metrica_atual;
 SELECT * FROM Metrica_Atual  WHERE mes  IN(1,2,3,4,5,6);
 
 INSERT INTO Metrica_atual  (fk_usuario, mes, metrica_tiro40y, metrica_3cones, metrica_shuttle_20, metrica_shuttle_60, metrica_supino) VALUES
@@ -97,8 +108,10 @@ UPDATE Metrica_atual SET metrica_tiro40y = 10.0
     
 SELECT * FROM Metrica_atual WHERE fk_usuario = 4;
 
+ SELECT mes, metrica_tiro40y FROM Metrica_atual WHERE mes IN (1,2,3,4,5,6) AND fk_usuario = 1;
 
-SELECT mes, metrica_tiro40y FROM Metrica_atual WHERE mes IN (1,2,3,4,5,6) AND fk_usuario = ${idUsuario};
+
+SELECT mes, metrica_tiro40y FROM Metrica_atual WHERE mes IN (1,2,3,4,5,6) AND fk_usuario = 3;
     
 SELECT nome_time, estado_time, região_time, cidade_time, formulario_time, categoria FROM TIMES WHERE região_time = 3;
 SELECT nome_time, estado_time, região_time, cidade_time, formulario_time, categoria FROM Usuario JOIN região ON região_usuario = idRegião JOIN TIMES ON região_time = idRegião WHERE idUsuario =4;
@@ -123,3 +136,52 @@ INSERT INTO TIMES (nome_time, estado_time, região_time, cidade_time,  formulari
 	('Galo Fa', 'Minas Gerais', 4, 'Belo Horizonte', 'sadasdasdasda',  'Masculino');
 INSERT INTO TIMES (nome_time, estado_time, região_time, cidade_time, formulario_time) VALUES
 	('SP Storm', 'São Paulo',  'sudeste', 'São Paulo', 'askdjaskjsda');
+    
+SELECT * FROM Metrica_atual WHERE fk_usuario = 1;
+SELECT * FROM Usuario;
+
+INSERT INTO Metrica_atual (mes, metrica_tiro40y, fk_usuario) VALUES
+	(1, 5.50, 1),
+	(2, 5.50, 1);
+    
+    
+    
+INSERT INTO Metrica_atual (mes, metrica_tiro40y, fk_usuario) VALUES
+	(5, 5.50, 1),
+	(6, 5.50, 1);
+INSERT INTO Metrica_atual (mes, metrica_tiro40y, fk_usuario) VALUES
+	(2, 4.50, 1),
+	(1, 4.50, 1);
+    
+    
+INSERT INTO Metrica_atual (mes, metrica_tiro40y, fk_usuario) VALUES
+	(2, 4.50, 1);
+    
+    INSERT INTO Metrica_atual (mes, metrica_tiro40y, fk_usuario) VALUES
+	(3, 4.50, 1);
+    
+    
+        INSERT INTO Metrica_atual (mes, metrica_tiro40y, fk_usuario) VALUES
+	(1, 4.50, 1),
+	(4, 4.50, 1);
+    
+    
+            INSERT INTO Metrica_atual (mes, metrica_tiro40y, fk_usuario) VALUES
+	(2, 4.50, 1),
+	(3, 4.50, 1);
+    
+            INSERT INTO Metrica_atual (mes, metrica_tiro40y, fk_usuario) VALUES
+	(5, 4.50, 1);
+    
+    
+SELECT * FROM Metrica_atual;
+DELETE FROM Metrica_Atual
+	WHERE idMetrica IN (2,3, 4, 5,  6, 7, 8, 9, 10, 11, 12 , 13 , 14, 15, 16,17,18, 19, 20 ,21 , 22, 23, 24, 25);
+    
+SELECT * FROM Metrica_atual;
+
+      UPDATE Metrica_atual SET metrica_tiro40y = 7.20
+    WHERE fk_usuario = 1 AND mes = 2;
+    
+SELECT * FROM Usuario;
+
